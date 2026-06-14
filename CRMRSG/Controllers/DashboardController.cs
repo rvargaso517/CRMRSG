76eb1a89e736a7ea65b5ebc24dd5d73817748f6a
Manrique31 <144976323+Manrique31@users.noreply.github.com>
2026-06-14 17:33:12 -0600

    HU-024: indicadores generales del CRM
    - Se implementó la HU-024: Indicadores generales del CRM.
    - Se conectó el Dashboard con la base de datos mediante Entity Framework.
    - Se agregaron contadores dinámicos para clientes, oportunidades, tareas y usuarios.
    - Se reemplazaron los valores estáticos del Dashboard por datos reales.
    - Se mantuvo el diseño original de la interfaz sin modificaciones visuales.

--- CRMRSG/Controllers/DashboardController.cs
+++ CRMRSG/Controllers/DashboardController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CRMRSG.EntityFramework;
 
 namespace CRMRSG.Controllers
 {
     public class DashboardController : Controller
     {
+        private crm_rsgEntities db = new crm_rsgEntities();
+
         // GET: Dashboard
         public ActionResult Index()
         {
+            ViewBag.TotalClientes = db.clientes.Count();
+            ViewBag.TotalOportunidades = db.oportunidades.Count();
+            ViewBag.TotalTareas = db.tareas.Count();
+            ViewBag.TotalUsuarios = db.usuarios.Count();
+
             return View();
         }
 
