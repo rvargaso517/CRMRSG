0daa39c0fe097ec320becc3ff8f65a35555def3b
ToledoZz <cerdasfallasj@gmail.com>
2026-06-14 20:08:11 -0600

    Historias completadas

---
+++ CRMRSG/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(CRMRSG.Startup))]
+
+namespace CRMRSG
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Este comando mágico es el que crea la ruta /signalr/hubs que te está dando 404
+            app.MapSignalR();
+        }
+    }
+}
